f3baaf14507affff820e770f5bffe9f4e602b234
Alexis Dominguez <t1mk1ll3r4ll@gmail.com>
2019-08-29 11:24:18 -0500

    added something then deleted it cuz it was to hard to implement
    i try to add a navigation bar but it was a pain in the ass to add so i said fuck it and revert everything back to normal and just changed the colors arround Kappa

--- app/src/main/java/com/UO/zoqueApp/menuPrincipal.java
+++ app/src/main/java/com/UO/zoqueApp/menuPrincipal.java
@@ -1,32 +1,18 @@
 package com.UO.zoqueApp;
 
 import android.content.Intent;
-import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Toast;
-
-
 import com.github.clans.fab.FloatingActionMenu;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FieldValue;
-import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class menuPrincipal extends AppCompatActivity {
-    int coinValue=0;
 
     List<Course> lstCourse;
     @Override
@@ -34,15 +20,10 @@ public class menuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-
         final FloatingActionMenu floatingMenu = findViewById(R.id.fab);
         RecyclerView rv = findViewById(R.id.recyclerView);
         lstCourse = new ArrayList<>();
         reciclerViewAdapter myAdapter = new reciclerViewAdapter(this, lstCourse);
-
-
-
-
 
         lstCourse.add(new Course("finanzas", "categoria: Monedas", "descricion: curso sobre manejo de finanzas", R.drawable.cursouno, "10"));
         lstCourse.add(new Course("networking", "categoria: relaciones", "descricion: curso sobre relaciones para el exito", R.drawable.cursodos , "20"));
@@ -52,7 +33,6 @@ public class menuPrincipal extends AppCompatActivity {
         lstCourse.add(new Course("traduccion", "categoria: lenguas", "descricion: curso sobre traduccion dah!", R.drawable.cursoseis, "60"));
         lstCourse.add(new Course("conversacion", "categoria: lenguas", "descricion: curso sobre conversaciones en otro idioma dah!", R.drawable.cursosiete,"70"));
         lstCourse.add(new Course("aprende a programar!", "categoria: IT", "descricion: curso basico de programacion", R.drawable.cursoocho,"80"));
-
 
         rv.setLayoutManager(new GridLayoutManager(this, 2));
         rv.setAdapter(myAdapter);
@@ -70,9 +50,7 @@ public class menuPrincipal extends AppCompatActivity {
             }
         });
     }
-    public void coinAdd(View view){
-        coinValue += 10;
-    }
+
     public void maraton(View view){
         Intent intent = new Intent(this,maraton.class);
         startActivity(intent);
