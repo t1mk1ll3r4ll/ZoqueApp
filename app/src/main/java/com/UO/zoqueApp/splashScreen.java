540bfa7c9d3eef147ea2700f8430c47b201f56bb
Alberto Alexis Dominguez Ruiz <t1mk1ll3r4ll@gmail.com>
2019-09-24 11:20:28 -0500

    new icon!
    minor UI Fixes and added new app icon looks mamalon AF uwu.
    and added full screen to the animation of the spash screen, fixed pressed buttons color to blue instead of red and i dont know what else i did :V

--- app/src/main/java/com/UO/zoqueApp/splashScreen.java
+++ app/src/main/java/com/UO/zoqueApp/splashScreen.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +19,11 @@ public class splashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
         mAuth = FirebaseAuth.getInstance();
         user=mAuth.getCurrentUser();
